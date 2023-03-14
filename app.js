09dfbf2ac45c787e31913cd777b35c70d061c11a
Mei Huang <100555326+meifhuang@users.noreply.github.com>
2023-03-13 23:54:59 -0400

    Set up env and seed data

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const Trail = require('./models/trail');
+require("dotenv").config();
 
-mongoose.connect('mongodb://localhost:27017/yelp-trail', {
+
+mongoose.connect(process.env.MONGODB, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
@@ -21,6 +24,11 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render("home");
+})
+
+app.get('/maketrail', async (req, res) => {
+    const trail = new Trail({title: 'My Trail', description:'cheap', image:'hi', difficulty:'hard', distance:'5.5'});
+   
 })
 
 app.listen(3000, ()=> {
