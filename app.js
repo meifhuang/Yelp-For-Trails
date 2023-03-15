b947e4786c04f2ced0564ea9e466887d78af281c
meifhuang <meifhuang5@gmail.com>
2023-03-15 00:31:54 -0400

    Implement add and style trail functionality

--- app.js
+++ app.js
@@ -19,28 +19,43 @@ db.on("error", console.error.bind(console, "connection err:"));
 db.once("open", () => {
     console.log("database connected");
 })
+
+
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
+
+
+//for trail difficulty options
+const difficulty = ['Easy', 'Moderate', 'Strenuous', 'Extremely strenuous']
 
 app.get('/', (req, res) => {
     res.render("home");
 })
 
 app.get('/trails', async (req, res) => {
-    const trail_list = await Trail.find({}); 
-    res.render('trails/index', {trail_list});
-})
-
-app.get('/trails/:id', async (req, res) => {
-   const trail = await Trail.findById(req.params.id);
-   res.render('trails/detail', {trail})
+    const trail_list = await Trail.find({});
+    res.render('trails/index', { trail_list });
 })
 
 app.get('/trails/new', (req, res) => {
-    res.render('trails/new');
+    res.render('trails/new', { difficulty });
 })
 
-app.listen(3000, ()=> {
+app.post('/trails', async (req, res) => {
+    const trail = new Trail(req.body);
+    await trail.save();
+    res.redirect(`/trails/${trail._id}`);
+})
+
+app.get('/trails/:id', async (req, res) => {
+    const trail = await Trail.findById(req.params.id);
+    res.render('trails/detail', { trail })
+})
+
+
+
+app.listen(3000, () => {
     console.log("Serving port 3000")
 })
