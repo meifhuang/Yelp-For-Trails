9fc05b45facca90c850e5b63352a2094558ab3fc
meifhuang <meifhuang5@gmail.com>
2023-03-17 15:29:18 -0400

    Create error page

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require('method-override');
 require("dotenv").config();
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError')
 
 
 mongoose.connect(process.env.MONGODB, {
@@ -46,6 +47,7 @@ app.get('/trails/new', (req, res) => {
 })
 
 app.post('/trails', catchAsync(async (req, res, next) => {
+    if (!req.body) throw new ExpressError('Invalid Campground Data', 400);
     const trail = new Trail(req.body);
     await trail.save();
     res.redirect(`/trails/${trail._id}`);
@@ -78,8 +80,9 @@ app.all('*', (req, res, next) => {
 })
 
 app.use((err, req, res, next) => {
-    const { status = 500, message = 'Something went wrong' } = err;
-    res.status(status).send(message);
+    const { status = 500 } = err;
+    if (!err.message) err.message = 'Something went wrong'
+    res.status(status).render('error', { err });
 })
 
 app.listen(3000, () => {
