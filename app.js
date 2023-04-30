5ebcc848b4a594f753237d003eb6a9d8fe709f79
Mei Huang <100555326+meifhuang@users.noreply.github.com>
2023-04-30 03:14:40 -0400

    Retest

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const mongoSanitize = require('express-mongo-sanitize'); 
-const MongoDBStore = require('connect-mongo');
+// const MongoDBStore = require('connect-mongo');
 
 
 
@@ -45,7 +45,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(mongoSanitize());
 
 // const store = MongoDBStore.create({
@@ -64,7 +63,7 @@ const sessConfig = {
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
-        secure: true,
+        // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7,
     }
